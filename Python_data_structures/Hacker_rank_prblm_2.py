56f1ad287a20c3c669eac63c2803262579e9f723
cs50summer <sameenaresume@gmail.com>
2024-05-10 11:24:29 -0500

    Update Hacker_rank_prblm_2.py

--- Python_data_structures/Hacker_rank_prblm_2.py
+++ Python_data_structures/Hacker_rank_prblm_2.py
@@ -1,19 +1,29 @@
 #!/bin/python3
 # hacker_ranker_prob_time_conversion
+# Complete the 'timeConversion' function below.
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+#timeConversion has the following parameter(s):
+#string s: a time in  hour format
+#Returns string: the time in  hour format
+#Input Format
+#A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+#Solution 
+# Spilt the string input and store only the hour number inside the 'hr' variable
+# If hour is less than 12  lets say 8 and ends with pm ie. night time , tehn add 12 to make it 20 and store it as 20.00 stripping off am or pm 
+# if hour is 12 then strip off only pm or am 
+# If hour is am then stip off only am 
+
 import math
 import os
 import random
 import re
 import sys
-#
-# Complete the 'timeConversion' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING s as parameter.
-#
+
 def timeConversion(s):
-    # Write your code here
-    #given input = 07:05:45PM
+    
     if s.lower().endswith("pm"):
         hr = s.split(':')[0]
         if int(hr) < 12:
@@ -22,16 +32,14 @@ def timeConversion(s):
             new_f = s.replace(hr, str(pms_conv)).lower().replace('pm',"")
             return new_f
         elif int(hr) == 12:
-            #print(s.lower().replace('pm',""))
             return s.lower().replace('pm',"")
     elif s.lower().endswith('am'):
         hr = s.split(':')[0]
         if int(hr) < 12:
-            #print(s.lower().replace('am', ""))
             return s.lower().replace('am',"")
         elif int(hr)==12:
-            #print(s.lower().replace(hr, '00').replace('am', ""))
-            return s.lower().replace(hr, '00').replace('am', "")
+           return s.lower().replace(hr, '00').replace('am', "")
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     s = input()
