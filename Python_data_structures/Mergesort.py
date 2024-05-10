972a4dd2e515007449c3f5d3cab83eee0bc9e855
cs50summer <sameenaresume@gmail.com>
2024-05-10 13:18:57 -0500

    Update Mergesort.py

--- Python_data_structures/Mergesort.py
+++ Python_data_structures/Mergesort.py
@@ -1,4 +1,13 @@
-ListNode mergeTwoLists(ListNode l1 , ListNode l2):
+# It breaks down an array into smaller subarrays, sorts them individually, and then merges 
+# them back together to create a sorted array. The code includes two main functions: merge, 
+# responsible for merging two subarrays, and mergeSort, which recursively divides and sorts 
+# the array. The merge function combines two sorted subarrays into a single sorted array. 
+# The mergeSort function recursively splits the array in half until each subarray has a single
+#  element, then merges them to achieve the final sorted result. 
+
+
+
+#ListNode mergeTwoLists(ListNode l1 , ListNode l2):
 
 
 
